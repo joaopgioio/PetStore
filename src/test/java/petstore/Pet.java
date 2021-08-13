429e754e00b3c6ae5e31760c4fea09a52da9ae02
Joao Paulo <joaopgioio@gmail.com>
2021-08-13 09:09:05 -0300

    Criei teste para consultar pet e incluir user

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -19,20 +19,20 @@ import static org.hamcrest.Matchers.contains;
 // 3 - Classe
 public class Pet {
     // 3.1 - Atributos
-    String uri = "https://petstore.swagger.io/v2/pet"; // endere√ßo da entidade Pet
+    String uri = "https://petstore.swagger.io/v2/pet"; // endereÁo da entidade Pet
 
-    // 3.2 - M√©todos e Fun√ß√µes
+    // 3.2 - MÈtodos e FunÁıes
     public String lerJson(String caminhoJson) throws IOException {
         return new String(Files.readAllBytes(Paths.get(caminhoJson)));
     }
 
     // Incluir - Create - Post
-    @Test // Identifica o metodo ou funcao como um Teste para o TestNG
+    @Test(priority = 1) // Identifica o metodo ou funcao como um Teste para o TestNG
     public void incluirPet() throws IOException {
         String jsonBody = lerJson("db/pet1.json");
 
         // Sintaxe Gherkin
-        // Dado - quando - Ent√£o
+        // Dado - quando - Ent„o
         // Given - When -  Then
 
         given()
@@ -46,32 +46,37 @@ public class Pet {
                 .statusCode(200)
                 .body("name", is("Suzi"))
                 .body("status", is("available"))
-                .body("category.name", is("dog"))
-                .body("tags.name", contains("Semana teste API"))
+                .body("category.name", is("AXEERAXFRV12323"))
+                .body("tags.name", contains("data"))
         ;
     }
-/*
     // Consulta - Read - Get
-    @Test // Identifica o metodo ou funcao como um Teste para o TestNG
-    public void consultarPet() throws IOException {
-        String jsonBody = lerJson("db/pet1.json");
+    @Test(priority = 2)
+    public void consultarPet(){
+        String petId = "198520211410";
 
         // Sintaxe Gherkin
-        // Dado - quando - Ent√£o
+        // Dado - quando - Ent„o
         // Given - When -  Then
 
+        String token =
         given()
                 .contentType("application/json") // comum em api rest - antigos era "text/xml"
                 .log().all()
-                .body(jsonBody)
+                //.body()
         .when()
-                .get(uri"198520211410")
+                .get(uri+"/"+petId)
         .then()
                 .log().all()
                 .statusCode(200)
                 .body("name", is("Suzi"))
+                .body("category.name", is("AXEERAXFRV12323"))
                 .body("status", is("available"))
 
+        .extract()
+                .path("category.name")
+
         ;
-    }*/
+        System.out.println("O token È: " +token);
+    }
 }
